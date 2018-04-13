e76cbc10f9b1ed0d172b53cbb0bebd744d887fa9
Oleh Kurachenko <oleh.kurachenko@gmail.com>
2018-04-13 13:55:56 +0300

    initial commit

---
+++ src/model/Model.py
@@ -0,0 +1,15 @@
+# 
+#   Created by Oleh Kurachenko
+#          aka okurache
+#   on 2018-04-12T23:59:46Z as a part of AxelrodModel4
+#   
+#   ask me      oleh.kurachenko@gmail.com , I'm ready to help
+#   GitHub      https://github.com/OlehKurachenko
+#   rate & CV   http://www.linkedin.com/in/oleh-kurachenko-6b025b111
+# 
+
+class Model:
+    # TODO write proper comments
+    """
+    A model
+    """
---
+++ src/model/__init__.py
@@ -0,0 +1,9 @@
+# 
+#   Created by Oleh Kurachenko
+#          aka okurache
+#   on 2018-04-12T23:58:34Z as a part of AxelrodModel4
+#   
+#   ask me      oleh.kurachenko@gmail.com , I'm ready to help
+#   GitHub      https://github.com/OlehKurachenko
+#   rate & CV   http://www.linkedin.com/in/oleh-kurachenko-6b025b111
+# 
